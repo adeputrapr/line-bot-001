f1c955ca3478b95f8d3149df12e9f0ac0fb68af5
adeputrapr <adeputrapr@gmail.com>
2020-03-08 10:42:07 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,4 @@
 <?php
-require_once __DIR__ . '/lineBot.php';
-
-$bot = new Linebot();
-$text = $bot->getMessageText();
-$bot->reply($text);
 
 if($userMessage == "contoh"){
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('ini adalah contoh text message');
