2c8c1a62a4593012e9f01fa5f18f5ab543893a52
adeputrapr <adeputrapr@gmail.com>
2020-03-08 10:34:24 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,11 +4,3 @@ require_once __DIR__ . '/lineBot.php';
 $bot = new Linebot();
 $text = $bot->getMessageText('HAI');
 $bot->reply($text);
-
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('<channel access token>');
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '<channel secret>']);
-
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
-$response = $bot->replyMessage('<replyToken>', $textMessageBuilder);
-
-echo $response->getHTTPStatus() . 'Hahaahah' . $response->getRawBody();
