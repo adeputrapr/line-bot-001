5f7da937d5d7d57c7770efc568eb5e4e34c0003b
adeputrapr <adeputrapr@gmail.com>
2020-03-08 10:30:10 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,3 +4,20 @@ require_once __DIR__ . '/lineBot.php';
 $bot = new Linebot();
 $text = $bot->getMessageText('HAI');
 $bot->reply($text);
+
+curl -v -X POST https://api.line.me/v2/bot/message/reply \
+-H 'Content-Type: application/json' \
+-H 'Authorization: Bearer {channel access token}' \
+-d '{
+    "replyToken":"nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
+    "messages":[
+        {
+            "type":"text",
+            "text":"Hello, user"
+        },
+        {
+            "type":"text",
+            "text":"May I help you?"
+        }
+    ]
+}'
