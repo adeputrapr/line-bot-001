902199f655225c7f6c4dbdab1a94ae7fb63085e9
adeputrapr <adeputrapr@gmail.com>
2020-03-08 10:41:16 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,5 +2,11 @@
 require_once __DIR__ . '/lineBot.php';
 
 $bot = new Linebot();
-$text = $bot->getMessageText('HAI');
+$text = $bot->getMessageText();
 $bot->reply($text);
+
+if($userMessage == "contoh"){
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('ini adalah contoh text message');
+$result = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
+return $result->getHTTPStatus() . ' ' . $result->getRawBody();     
+}
